cd738c66bb8fb8eb92c74e42c166aa58e4d2658f
euler.sistemas <euler.sistemas@gmail.com>
2025-03-13 12:01:43 -0300

    add missing providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ColorThemeProvider } from "@/components/color-theme-provider";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <ColorThemeProvider>{children}</ColorThemeProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
